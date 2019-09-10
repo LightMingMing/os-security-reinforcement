3fc203ef689d7475751b8c3f1e9cbc9ccc29e978
LightMingMing <1849901944@qq.com>
2019-09-10 23:19:23 +0800

    Tomorrow's plan

---
+++ os_software_install.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+"""
+操作系统基础软件安装
+1. 基础环境: vim, gcc, iftop, firewall-cmd, nslookup, iftop, iperf, telnet, zip, tar, lvm2, nmcli
+2. 运行环境: nginx, java
+3. 监控: zabbix
+
+author: 赵明明
+"""
+
+# TODO
+# 1. 配置yum源代理
+# 2. 安装各个所需软件, 需要确定各个软件的安装方式
+# 3. nmcli配置DNS服务器
+# 4. 系统时间同步，日期同步
+# 5. 系统兼容性 CentOS 6、CentOS 7、Ubuntu
